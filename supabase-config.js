0159c9edc77f5b1792541f4cd360b7223168fcb8
ranadeep-banik137 <42244847+ranadeep-banik137@users.noreply.github.com>
2026-01-31 01:15:56 +0530

    Big modification for security

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+const S_URL = 'https://wbhcvcsdpdandaxidnlg.supabase.co';
+const S_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiaGN2Y3NkcGRhbmRheGlkbmxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5NjMwMjksImV4cCI6MjA4MTUzOTAyOX0.n8R1OpFxilWbDfteODc-lyVF_qvntyRnJNVM3g9xCWs'; 
+const sb = supabase.createClient(S_URL, S_KEY);
